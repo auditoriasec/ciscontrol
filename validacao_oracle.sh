b0ab4fbcf296b92f81445c8eccfe8deb9a8466b5
devsecrafa <rafael.secauditor@gmail.com>
2021-11-01 00:10:59 -0400

    script - automação de validação de hardening - Oracle 11G

---
+++ validacao_oracle.sh
@@ -0,0 +1,10 @@
+##########################################################
+#                                                        #
+# Autor: Rafael Silva                                    #
+# Linkedin: https://www.linkedin.com/in/rafaelsouzasilva #
+# Auditor Sênior | Cyber Security                        #
+# Data: 31/10/2021                                       #
+#                                                        #
+##########################################################
+
+
